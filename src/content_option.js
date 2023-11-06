2d6073ecf77b3a6a0a0b3be59fd34b1bdbbc67e8
JLaikows <joshua@laikowski.com>
2023-11-05 19:53:57 -0500

    initgit

---
+++ src/content_option.js
@@ -0,0 +1,113 @@
+const logotext = "JOSHUA LAIKOWSKI";
+const meta = {
+    title: "Joshua Laikowski",
+    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+};
+
+const introdata = {
+    title: "I’m Joshua Laikowski",
+    animated: {
+        first: "am a full stack dev",
+        second: "love coding",
+        third: "love exploring new tech",
+    },
+    description: "Full Stack engineer from Brooklyn, New York ",
+    your_img_url: "https://media.licdn.com/dms/image/C5603AQEWjaDa3r-mng/profile-displayphoto-shrink_800_800/0/1655820574927?e=1704931200&v=beta&t=kVVNz1gdDnJ60-dp-v2p9DhTEg1aiuZBOYxkNV1okKI",
+};
+
+const dataabout = {
+    title: "A bit about myself",
+    aboutme: "I'm a software enigneer originally from Sayreville, New Jersey. From a young age I found my self fasinated by how technology worked, eventually leading me to video games and more. As I got older, and my muse diverged to music, I still found myself trying to build little applications to make my music reoording experience easier. Then, during the beggining of the pandemic, I went to App Academy to learn full stack engineering, and the rest is history",
+};
+const worktimeline = [{
+        jobtitle: "Software Engineer III",
+        where: "Better Mortgage",
+        date: "2022 - 2023",
+    }
+];
+
+const skills = [
+    {
+        name: "Javascript",
+        value: 95,
+    },
+    {
+        name: "Typescript",
+        value: 95,
+    },
+    {
+        name: "React",
+        value: 90,
+    },
+    {
+        name: "Python",
+        value: 70,
+    },
+    {
+        name: "Golang",
+        value: 40
+    },
+    {
+        name: "Ember",
+        value: 50,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Mobile Apps",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Wordpress Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://media.licdn.com/dms/image/sync/C4D27AQG8f5zDelyiyQ/articleshare-shrink_800/0/1698980106682?e=1699837200&v=beta&t=IH6hNodcKOWKiULFa7uQTAwcfF53tBhGBona-lnjcoA",
+        description: "FacePalm - a facebook clone",
+        link: "https://github.com/ShuaLaik/FacePalm",
+    },
+    {
+        img: "https://user-images.githubusercontent.com/63963324/150466955-c92744a9-ee2a-4f79-93ca-5066bd141ad3.png",
+        description: "Landmarked - track your trips with photos and more!",
+        link: "https://github.com/ShuaLaik/Landmarked",
+    },
+    {
+        img: "https://i.postimg.cc/mrYvX0zZ/Screenshot-2023-11-05-194632.png",
+        description: "Better Mortgage - Software Engineer III",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "joshua@laikowski.com",
+    YOUR_FONE: "(732)609-3243",
+    description: "Please reach out if you have any questions/requests. I'll get back to you as soon as possible!",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_gicyo97",
+    YOUR_TEMPLATE_ID: "template_ge6nmiw",
+    YOUR_USER_ID: "2-uByNV-ZouzgtPzO",
+};
+
+const socialprofils = {
+    github: "https://github.com/JLaikows",
+    linkedin: "https://www.linkedin.com/in/joshualaikowski/",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
